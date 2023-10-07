20fd5e7f355bbf75a658753586564ad5a9e46423
阿牛 <hpraniu@163.com>
2023-10-07 21:15:03 +0800

    跳转刷新时间。

--- local_taobao1.py
+++ local_taobao1.py
@@ -192,12 +192,12 @@ class Taobao:
                         while True:
                             if count > 30:
                                 raise Exception('Error')
-                            # submit_order = driver.find_element(By.LINK_TEXT, "提交订单")
-                            # if submit_order:
-                            #     # print(f'拍下商品时间: ========={datetime.fromtimestamp(self.get_taobao_time() / 1000)}')
-                            #     submit_order.click()
-                            #     print(f'拍下商品时间: ========={datetime.now()}')
-                            #     break
+                            submit_order = driver.find_element(By.LINK_TEXT, "提交订单")
+                            if submit_order:
+                                # print(f'拍下商品时间: ========={datetime.fromtimestamp(self.get_taobao_time() / 1000)}')
+                                submit_order.click()
+                                print(f'拍下商品时间: ========={datetime.now()}')
+                                break
                             driver.refresh()
                             count += 1
                             print(f'第{count}次刷新====================={datetime.now()}')
@@ -207,20 +207,20 @@ class Taobao:
                         print(f'========抢购失败============')
                         break
 
-                    # # 进入到付款的界面
-                    # try:
-                    #     webdriver_wait = WebDriverWait(driver, 20, 0.1)
-                    #     payment_page = webdriver_wait.until(
-                    #         EC.element_to_be_clickable((By.XPATH, '//*[@id="channels"]/div/li/div')))
-                    #     print(payment_page.text)
-                    #     # print(f'付款页面时间: ========={datetime.fromtimestamp(self.get_taobao_time() / 1000)}')
-                    #     print(f'付款页面时间: ========={datetime.now()}')
-                    #     driver.quit()
-                    #     break
-                    # except Exception:
-                    #     print(f'========付款页面未显示============')
-                    #     driver.quit()
-                    #     break
+                    # 进入到付款的界面
+                    try:
+                        webdriver_wait = WebDriverWait(driver, 20, 0.1)
+                        payment_page = webdriver_wait.until(
+                            EC.element_to_be_clickable((By.XPATH, '//*[@id="channels"]/div/li/div')))
+                        print(payment_page.text)
+                        # print(f'付款页面时间: ========={datetime.fromtimestamp(self.get_taobao_time() / 1000)}')
+                        print(f'付款页面时间: ========={datetime.now()}')
+                        driver.quit()
+                        break
+                    except Exception:
+                        print(f'========付款页面未显示============')
+                        driver.quit()
+                        break
             else:
                 time.sleep(1)
                 initital_time = int(time.time() * 1000)
